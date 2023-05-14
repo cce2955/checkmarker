0c636e58e973cc2347e643cd3888b86fda699bc4
Christopher Everett <cce29555@gmail.com>
2023-05-14 08:58:29 -0400

    refactored flipswitch functionality, now persists across tabs

--- content.js
+++ content.js
@@ -1,41 +1,22 @@
-chrome.storage.sync.get("automaticCheckAllEnabled", function (data) {
-  var automaticCheckAllEnabled = data.automaticCheckAllEnabled;
-
-  if (automaticCheckAllEnabled) {
-    var inputs = document.getElementsByTagName("input");
-    for (var i = 0; i < inputs.length; i++) {
-      var input = inputs[i];
-      if (input.type === "checkbox") {
-        input.checked = true;
-      }
-    }
+chrome.storage.sync.get("autoCheckAll", function(data) {
+  if (data.autoCheckAll) {
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(checkbox => {
+      checkbox.checked = true;
+    });
   }
 });
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "checkAll") {
-    chrome.storage.sync.get("automaticCheckAllEnabled", function (data) {
-      var automaticCheckAllEnabled = data.automaticCheckAllEnabled;
-
-      if (automaticCheckAllEnabled) {
-        var inputs = document.getElementsByTagName("input");
-        for (var i = 0; i < inputs.length; i++) {
-          var input = inputs[i];
-          if (input.type === "checkbox") {
-            input.checked = true;
-          }
-        }
-      }
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  if (changes.autoCheckAll && changes.autoCheckAll.newValue) {
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(checkbox => {
+      checkbox.checked = true;
     });
-  } else if (message.action === "setText") {
-    var inputs = document.getElementsByTagName("input");
-    var text = message.text;
-    for (var i = 0; i < inputs.length; i++) {
-      var input = inputs[i];
-      if (input.type === "text") {
-        input.value = text;
-      }
-    }
-    console.log("Set text to: " + text);
+  } else {
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(checkbox => {
+      checkbox.checked = false;
+    });
   }
 });
--- popup.js
+++ popup.js
@@ -1,66 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Get the "check-all" button
-  var checkAllButton = document.getElementById("check-all");
+document.addEventListener("DOMContentLoaded", function() {
+  let flipSwitch = document.getElementById("flip-switch");
 
-  // Get the flip switch checkbox
-  var flipSwitch = document.getElementById("flip-switch");
-
-  // Add a click event listener to the "check-all" button
-  checkAllButton.addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var tabId = tabs[0].id;
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabId },
-          files: ["content.js"],
-        },
-        function () {
-          chrome.tabs.sendMessage(tabId, {
-            action: "checkAll"
-          });
-        }
-      );
-    });
+  flipSwitch.addEventListener("change", function() {
+    chrome.storage.sync.set({ autoCheckAll: flipSwitch.checked });
   });
+});
 
-  // Add a change event listener to the flip switch checkbox
-  flipSwitch.addEventListener("change", function () {
-    if (this.checked) {
-      // Set a variable to true when the flip switch is on
-      chrome.storage.sync.set({ automaticCheckAllEnabled: true }, function() {
-        console.log('Automatic check-all enabled');
-      });
-    } else {
-      // Set a variable to false when the flip switch is off
-      chrome.storage.sync.set({ automaticCheckAllEnabled: false }, function() {
-        console.log('Automatic check-all disabled');
-      });
-    }
-  });
-
-  // Listen for updates to the active tab
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    // Check if the flip switch is on
-    chrome.storage.sync.get(['automaticCheckAllEnabled'], function(result) {
-      if (result.automaticCheckAllEnabled) {
-        // Automatically apply the checkAll code when a new page is loaded and the flip switch is on
-        chrome.scripting.executeScript(
-          {
-            target: { tabId: tab.id },
-            files: ["content.js"],
-          },
-          function () {
-            chrome.tabs.sendMessage(tab.id, {
-              action: "checkAll"
-            });
-          }
-        );
-      }
-    });
-  });
-
-  // Load the status of the flip switch from storage
-  chrome.storage.sync.get(['automaticCheckAllEnabled'], function(result) {
-    flipSwitch.checked = result.automaticCheckAllEnabled;
-  });
+chrome.storage.sync.get("autoCheckAll", function(data) {
+  flipSwitch.checked = data.autoCheckAll || false;
 });
